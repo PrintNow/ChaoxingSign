c9d0953083d99493b8ca8235da1876fad0ed17e0
chuwen <chenwenzhou@aliyun.com>
2020-05-01 21:17:13 +0800

    Initialize project

---
+++ lib/Functions.php
@@ -0,0 +1,75 @@
+<?php
+define("LOGIN_API", "http://i.chaoxing.com/vlogin?userName=%s&passWord=%s");//登录接口
+define("COURSE_LIST", "http://mooc1-2.chaoxing.com/visit/interaction");//获取课程 courseId、classId
+define("TASK_ID", "http://mobilelearn.chaoxing.com/widget/pcpick/stu/index?courseId=%s&jclassId=%s");//获取任务 ID
+define("SIGN", "http://mobilelearn.chaoxing.com/widget/sign/pcStuSignController/preSign?activeId=%s&classId=%s&courseId=%s");//获取任务 ID
+
+/**
+ * 判断是否为命令行模式
+ * @return bool
+ */
+function is_cli(){
+    return preg_match("/cli/i", php_sapi_name()) ? true : false;
+}
+
+/**
+ * 返回课程列表
+ * @author Chuwen<wenzhouchan@gmail.com>
+ * @param $html
+ * @return array
+ */
+function get_course_list($html)
+{
+    $data = selector::select($html, 'li[style="position:relative"]', 'css');
+
+    if ($data === NULL) return [];
+
+    $class_list = [];
+    foreach ($data as $k => $v) {
+        $class_list[] = [
+            'courseId' => selector::select($v, '@<input type="hidden" name="courseId" value="(.*?)" />@', 'regex'),
+            'classId' => selector::select($v, '@<input type="hidden" name="classId" value="(.*?)" />@', 'regex'),
+            'title' => selector::select($v, '.clearfix > a', 'css'),
+        ];
+    }
+
+    return $class_list;
+}
+
+
+function get($parameter, $default = null, $filter = 'trim')
+{
+    return isset($_GET[$parameter]) ? $filter($_GET[$parameter]) : $default;
+}
+
+/**
+ * @author Chuwen<wenzhouchan@gmail.com>
+ * @param $parameter
+ * @param null $default
+ * @param string $filter
+ * @return null
+ */
+function post($parameter, $default = null, $filter = 'trim')
+{
+    return isset($_POST[$parameter]) ? $filter($_POST[$parameter]) : $default;
+}
+
+
+/**
+ *curl get请求
+ */
+function curl_get($url, $cookie_jar)
+{
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, $url);//登陆后要从哪个页面获取信息
+    curl_setopt($curl, CURLOPT_HEADER, false);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4121.0 Safari/537.36 Edg/84.0.495.2");
+    curl_setopt($curl, CURLOPT_TIMEOUT, 10);
+    curl_setopt($curl, CURLOPT_COOKIEJAR, $cookie_jar); //保存返回的Cookie
+    curl_setopt($curl, CURLOPT_COOKIEFILE, $cookie_jar); //读取现有Cookie
+    $content = curl_exec($curl);
+    curl_close($curl);
+
+    return $content;
+}
---
+++ main.php
@@ -0,0 +1,123 @@
+<?php
+include_once __DIR__ . "/lib/Functions.php";
+include_once __DIR__ . "/lib/Selector.php";
+
+$now = date('Hi');
+
+if($now >= '0800' && $now <= '2200'){}else{
+    die("仅能在 每天 早上8点到晚上22点之间 签到".PHP_EOL);
+}
+
+if (is_cli() && isset($argv)) {
+    $param = getopt('A:P:');
+    if(!isset($param['A']) || !isset($param['P'])){
+        die("使用方法：php main.php -A 你的账号 -P 你的密码".PHP_EOL.PHP_EOL
+            ."例如：php main.php -A 1008611 -P zgyd1008611".PHP_EOL
+            ."或者(如果你的账号含有特殊字符，建议使用双引号)：php main.php -A \"1008611\" -P \"zgyd1008611\"".PHP_EOL);
+    }
+    $account = $param['A'];
+    $password = $param['P'];
+}else{
+    $account = get('account');
+    $password = get('password');
+}
+
+$jar_path = __DIR__ . "/cookie/{$account}.cookie";//保存 Cookie
+
+
+//如果存在 cookie 文件，先判断 cookie 是否过期
+if (file_get_contents($jar_path)) {
+    goto getCourseList;
+}
+goto takeLogin;//去执行登录操作
+
+
+
+
+//获取课程列表
+getCourseList:
+$html = curl_get(COURSE_LIST, $jar_path);
+$course_list = get_course_list($html);//获取课程列表
+
+if (isset($course_list[0])) {
+    goto getTaskID;//获取任务ID
+}
+goto takeLogin;//去执行登录操作
+
+
+
+
+//获取任务ID
+getTaskID:
+$taskID = [];
+foreach ($course_list as $val) {
+    $html = curl_get(sprintf(TASK_ID, $val['courseId'], $val['classId']), $jar_path);
+    $res = selector::select($html, '#startList > div', 'css');
+
+    if (isset($res[0])) {
+        if (!is_array($res)) continue;
+        foreach ($res as $k => $v) {
+            $d = selector::select($v, '@activeDetail(.*?)"@', 'regex');
+            $d = str_replace(['(', ')', ''], '', $d);
+            $d = explode(",", $d);
+
+            if (intval($d[1]) === 2) {
+                $taskID[] = [
+                    $val['courseId'],//课程ID
+                    $val['classId'],//班级ID
+                    $d[0],//签到任务ID
+                    $val['title']
+                ];
+            }
+        }
+    }
+}
+
+if (count($taskID) > 0) {
+    goto doTask;
+}
+
+//preg_match("/进行中(\d+)/", str_replace(['(', ')', "\n", "\t"], '', $html), $matches);
+//if(isset($matches[1])){
+//    if(intval($matches[1]) > 0){
+//        echo '有任务';
+//    }
+//}
+echo "没有待签到的任务".PHP_EOL;
+die;
+
+
+//执行任务
+doTask:
+foreach ($taskID as $k => $v) {
+    echo "正在签到：{$v[3]}...";
+    curl_get(sprintf(SIGN, $v[2], $v[1], $v[0]), $jar_path);
+    echo "应该签到成功".PHP_EOL.PHP_EOL;
+}
+die;
+
+
+//登陆账号
+takeLogin:
+$login_data = json_decode(curl_get(sprintf(LOGIN_API, $account, $password), $jar_path), true);
+
+if ($login_data === null) {
+    echo "登陆失败，超星 API 错误".PHP_EOL;
+    unlink($jar_path);//删除文件，避免产生大量的垃圾文件
+    die;
+}
+
+if (!isset($login_data['success'])) {
+    echo "登陆失败，超星 API 错误".PHP_EOL;
+    unlink($jar_path);//删除文件，避免产生大量的垃圾文件
+    die;
+}
+
+if (!$login_data['success']) {
+    echo $login_data['errorMsg'].PHP_EOL;
+    unlink($jar_path);//删除文件，避免产生大量的垃圾文件
+    die;
+}
+
+echo "登陆成功".PHP_EOL;
+goto getCourseList;
