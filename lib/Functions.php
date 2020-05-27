c6127ddbf3b5468e1e2fb8eba943b54d6b02bb50
Chuwen <chenwenzhou@aliyun.com>
2020-05-27 22:59:56 +0800

    Add Server酱 微信推送

--- lib/Functions.php
+++ lib/Functions.php
@@ -1,23 +1,60 @@
 <?php
 //define("LOGIN_API", "http://i.chaoxing.com/vlogin?userName=%s&passWord=%s");//~~登录接口~~ 该接口目前失效
 define("LOGIN_API", "https://passport2-api.chaoxing.com/v11/loginregister?uname=%s&code=%s");//登录接口
-define("COURSE_LIST", "http://mooc1-2.chaoxing.com/visit/interaction");//获取课程 courseId、classId
+
+define("COURSE_LIST", "http://mooc1-api.chaoxing.com/mycourse/backclazzdata");//获取课程 courseId、classId
+define("COURSE_LIST_OLD", "http://mooc1-2.chaoxing.com/visit/interaction");//<旧方法>获取课程 courseId、classId
+
 define("TASK_ID", "http://mobilelearn.chaoxing.com/widget/pcpick/stu/index?courseId=%s&jclassId=%s");//获取任务 ID
-define("SIGN", "http://mobilelearn.chaoxing.com/widget/sign/pcStuSignController/preSign?activeId=%s&classId=%s&courseId=%s");//获取任务 ID
+
+define("SIGN_API", "https://mobilelearn.chaoxing.com/pptSign/stuSignajax?activeId=%s");//获取任务 ID
+define("SIGN_API_OLD", "http://mobilelearn.chaoxing.com/widget/sign/pcStuSignController/preSign?activeId=%s&classId=%s&courseId=%s");//<旧方法>获取任务 ID
+
+/**
+ * SERVER CHAN 微信推送
+ * @param string $text         消息标题，最长为256，必填。
+ * @param string $desp         消息内容，最长64Kb，可空，支持MarkDown。
+ * @param string $key          获取方式：http://sc.ftqq.com/?c=code
+ * @return false|string
+ */
+function sc_send($text='', $desp = '', $key = '')
+{
+    $postdata = http_build_query(
+        array(
+            'text' => $text,
+            'desp' => $desp
+        )
+    );
+
+    $opts = array('http' =>
+        array(
+            'method' => 'POST',
+            'timeout' => 5,//超时时间 5秒
+            'header' => 'Content-type: application/x-www-form-urlencoded',
+            'content' => $postdata
+        )
+    );
+    $context = stream_context_create($opts);
+
+    $result = json_decode(@file_get_contents('https://sc.ftqq.com/' . $key . '.send', false, $context), true);
+    return $result;
+}
 
 /**
  * 判断是否为命令行模式
  * @return bool
  */
-function is_cli(){
+function is_cli()
+{
     return preg_match("/cli/i", php_sapi_name()) ? true : false;
 }
 
+
 /**
- * 返回课程列表
- * @author Chuwen<wenzhouchan@gmail.com>
+ * 获取课程列表 <旧方法，保留备用>
  * @param $html
  * @return array
+ * @author Chuwen<wenzhouchan@gmail.com>
  */
 function get_course_list($html)
 {
@@ -44,11 +81,11 @@ function get($parameter, $default = null, $filter = 'trim')
 }
 
 /**
- * @author Chuwen<wenzhouchan@gmail.com>
  * @param $parameter
  * @param null $default
  * @param string $filter
  * @return null
+ * @author Chuwen<wenzhouchan@gmail.com>
  */
 function post($parameter, $default = null, $filter = 'trim')
 {
