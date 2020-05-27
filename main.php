0f534758e74ee038fbb1a32da96c98e858f77e1c
Chuwen <chenwenzhou@aliyun.com>
2020-05-27 23:03:05 +0800

    Fixed #1, Modify 登录、获取课程、签到 API, Add 手势、位置、二维码一键签到, Add Server酱 微信推送，需要配置 config.php 文件

--- main.php
+++ main.php
@@ -1,12 +1,13 @@
 <?php
 include_once __DIR__ . "/lib/Functions.php";
 include_once __DIR__ . "/lib/Selector.php";
+include_once __DIR__ . "/Config.php";
 
 $now = date('Hi');
 
-if($now >= '0800' && $now <= '2200'){}else{
-    die("仅能在 每天 早上8点到晚上22点之间 签到".PHP_EOL);
-}
+//if($now >= '0800' && $now <= '2200'){}else{
+//    die("仅能在 每天 早上8点到晚上22点之间 签到".PHP_EOL);
+//}
 
 if (is_cli() && isset($argv)) {
     $param = getopt('A:P:');
@@ -22,31 +23,58 @@ if (is_cli() && isset($argv)) {
     $password = get('password');
 }
 
-$jar_path = __DIR__ . "/cookie/{$account}.cookie";//保存 Cookie
+$jar_path = __DIR__ . "/cookie/{$account}.cookie";//保存 Cookie 的路径
+$signed_path = __DIR__ . "/cookie/{$account}.signed";//保存 已经签到完成 的路径
 
 
 //如果存在 cookie 文件，先判断 cookie 是否过期
-if (file_get_contents($jar_path)) {
+if (file_exists($jar_path)) {
     goto getCourseList;
 }else{
     //不存在 cookie 文件，则新建一个
     file_put_contents($jar_path, "#新建一个保存cookie文件");
 }
-goto takeLogin;//去执行登录操作
+goto takeLogin;//执行登陆
 
 
 
 
 //获取课程列表
 getCourseList:
-$html = curl_get(COURSE_LIST, $jar_path);
-$course_list = get_course_list($html);//获取课程列表
+$getCourseListRes = json_decode(curl_get(COURSE_LIST, $jar_path), true);
 
-if (isset($course_list[0])) {
-    goto getTaskID;//获取任务ID
+if(!isset($getCourseListRes['channelList'])){
+    die("获取课程列表失败，请稍后再试。多次出现此问题请前往 https://github.com/PrintNow/ChaoxingSign 提交 Issues");
 }
-goto takeLogin;//去执行登录操作
 
+$course_list = [];
+foreach ($getCourseListRes['channelList'] as $v){
+    //TODO: 后续再考虑字段可能不存在的情况，能用就行
+    /*
+    $title = '未知课程名';
+    $teacherName = '未知教师名';
+
+    if(isset($v['content']['course']['data'][0])){
+        if(isset($v['content']['course']['data'][0]['name'])){
+            $title = $v['content']['course']['data'][0]['name'];
+        }
+        if(isset($v['content']['course']['data'][0]['teacherfactor'])){
+            $teacherName = $v['content']['course']['data'][0]['teacherfactor'];
+        }
+    }
+    */
+
+    if(!isset($v['content']['course']['data'][0]['id'])) continue;
+
+    $course_list[] = [
+        'courseId' => $v['content']['course']['data'][0]['id'],
+        'classId' => $v['content']['id'],
+        'title' => $v['content']['course']['data'][0]['name'],
+        'teacherName' => $v['content']['course']['data'][0]['teacherfactor'],
+    ];
+}
+
+goto getTaskID;//获取任务 ID
 
 
 
@@ -69,7 +97,8 @@ foreach ($course_list as $val) {
                     $val['courseId'],//课程ID
                     $val['classId'],//班级ID
                     $d[0],//签到任务ID
-                    $val['title']
+                    $val['title'],//课程名
+                    $val['teacherName'],//教师名
                 ];
             }
         }
@@ -77,26 +106,61 @@ foreach ($course_list as $val) {
 }
 
 if (count($taskID) > 0) {
+
+    if (!file_exists($signed_path)) {
+        file_put_contents($signed_path, "#新建一个 已经签到完的活动 文件");
+    }
+
     goto doTask;
 }
 
-//preg_match("/进行中(\d+)/", str_replace(['(', ')', "\n", "\t"], '', $html), $matches);
-//if(isset($matches[1])){
-//    if(intval($matches[1]) > 0){
-//        echo '有任务';
-//    }
-//}
+file_put_contents($signed_path, "");//没有签到任务了，将其置为空
 echo "没有待签到的任务".PHP_EOL;
 die;
 
 
 //执行任务
 doTask:
+$activeBlackList = explode("\n", file_get_contents($signed_path));
+
+$msgTmp = "";
 foreach ($taskID as $k => $v) {
-    echo "正在签到：{$v[3]}...";
-    curl_get(sprintf(SIGN, $v[2], $v[1], $v[0]), $jar_path);
-    echo "应该签到成功".PHP_EOL.PHP_EOL;
+
+    //该签到任务已经签到了，不需要再次重复签到
+    if(in_array($v[2], $activeBlackList)) continue;
+
+    echo $_1 = "正在签到：{$v[4]}@{$v[3]}";
+    $signRes = trim(curl_get(sprintf(SIGN_API, $v[2]), $jar_path));//签到结果
+
+    echo $_2 = PHP_EOL."[".date("Y-m-d H:i:s")."]";
+    if($signRes === "success" || $signRes === "您已签到过了"){
+        //改签到加入 签到黑名单，以避免重复签到
+        file_put_contents($signed_path, "\n".$v[2], FILE_APPEND);
+    }
+
+    echo $_3 = str_replace("success", "签到成功", $signRes).PHP_EOL.PHP_EOL;
+
+    $msgTmp .= $_1.$_2.$_3;
 }
+
+//Server酱 微信推送
+if(!empty($msgTmp) && SERVER_CHAN_STATE && isset($config['SERVER_CHAN'][$account])){
+    if($config['SERVER_CHAN'][$account]['state'] === true){
+        $req = sc_send("超星自动签到成功", $msgTmp, $config['SERVER_CHAN'][$account]['SCKEY']);
+
+        if(!isset($req['errmsg'])){
+            die("Server酱 推送失败，可能是你没有配置 SCKEY，请检查");
+        }
+
+        if($req['errmsg'] === 'success'){
+            echo "Server酱 消息推送成功".PHP_EOL;
+        }else{
+            echo $req['errmsg'];
+        }
+    }
+}
+
+echo "没有待签到的任务".PHP_EOL;
 die;
 
 
@@ -104,23 +168,14 @@ die;
 takeLogin:
 $login_data = json_decode(curl_get(sprintf(LOGIN_API, $account, $password), $jar_path), true);
 
-if ($login_data === null) {
-    echo "登陆失败，超星 API 错误".PHP_EOL;
-    unlink($jar_path);//删除文件，避免产生大量的垃圾文件
-    die;
+if (!isset($login_data['status'])) {
+    die("登陆失败，原因：API 错误，请再次尝试。多次出现此问题请前往 https://github.com/PrintNow/ChaoxingSign 提交 Issues");
 }
 
-if (!isset($login_data['success'])) {
-    echo "登陆失败，超星 API 错误".PHP_EOL;
-    unlink($jar_path);//删除文件，避免产生大量的垃圾文件
-    die;
+if($login_data['status'] !== true){
+    unlink($jar_path);//删除临时创建的 cookie 文件，避免产生大量的垃圾文件
+    die("登陆失败，原因：{$login_data['mes']}".PHP_EOL);
 }
 
-if (!$login_data['success']) {
-    echo $login_data['errorMsg'].PHP_EOL;
-    unlink($jar_path);//删除文件，避免产生大量的垃圾文件
-    die;
-}
-
-echo "登陆成功".PHP_EOL;
-goto getCourseList;
+echo "登陆成功，正在尝试签到...".PHP_EOL;
+goto getCourseList;//获取课程列表
