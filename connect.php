24b31a81a0475eee386db45e3dffab7b553b2cd1
BA7JCM <1677128936@qq.com>
2021-05-09 00:00:09 +0800

    连接数据库

--- connect.php
+++ connect.php
@@ -1,5 +1,5 @@
 <?php
 // 连接MySQL数据库
-$connect = mysqli_connect('localhost', 'cxqd','Yeyvxvan20001229','cxqd');
+$connect = mysqli_connect('数据库连接地址，一般是localhost', '这里改成你自己连接数据库的用户名','数据库密码','数据库名');
 mysqli_query($connect , "set names utf8");
 ?>
