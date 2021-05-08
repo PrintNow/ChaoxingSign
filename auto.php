bc7be4dac4a153358f81bf3519abe1c782eeebf1
BA7JCM <1677128936@qq.com>
2021-05-08 23:49:34 +0800

    自动签到

---
+++ auto.php
@@ -0,0 +1,19 @@
+<?PHP
+include 'connect.php';
+$sql = "SELECT * FROM list";
+$list = mysqli_query($connect,$sql);
+while($run = mysqli_fetch_assoc($list)){
+    $account = $run['tel'];
+    $password = $run['password'];
+    $name = $run['name'];
+    $url = "https://cx.ba7jcm.live/main.php?account=$account&password=$password";
+    $single = file_get_contents($url);
+    if($single){
+        echo $single;
+        echo "<br>";
+        echo $name."尝试签到完成<br><hr>";
+    }else{
+        echo $name."尝试签到失败<br><hr>";
+    }
+}
+?>
