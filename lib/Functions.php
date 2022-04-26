16d630c99885bc212e57c01f38391a2402b5682c
Vincent <i@yyt.moe>
2022-04-26 11:20:00 +0800

    修复由于超星更新导致的签到失败和Bark推送失败的问题

--- lib/Functions.php
+++ lib/Functions.php
@@ -5,7 +5,8 @@ define("LOGIN_API", "https://passport2-api.chaoxing.com/v11/loginregister?uname=
 define("COURSE_LIST", "http://mooc1-api.chaoxing.com/mycourse/backclazzdata");//获取课程 courseId、classId
 define("COURSE_LIST_OLD", "http://mooc1-2.chaoxing.com/visit/interaction");//<旧方法>获取课程 courseId、classId
 
-define("TASK_ID", "http://mobilelearn.chaoxing.com/widget/pcpick/stu/index?courseId=%s&jclassId=%s");//获取任务 ID
+define("TASK_ID", "https://mobilelearn.chaoxing.com/ppt/activeAPI/taskactivelist?courseId=%s&classId=%s");//获取任务 ID
+define("TASK_ID_OLD", "http://mobilelearn.chaoxing.com/widget/pcpick/stu/index?courseId=%s&jclassId=%s");//获取任务 ID
 
 define("SIGN_API", "https://mobilelearn.chaoxing.com/pptSign/stuSignajax?activeId=%s");//获取任务 ID
 define("SIGN_API_OLD", "http://mobilelearn.chaoxing.com/widget/sign/pcStuSignController/preSign?activeId=%s&classId=%s&courseId=%s");//<旧方法>获取任务 ID
@@ -82,15 +83,9 @@ function tg_send($chatID, $message, $token)
  */
 function bark_send($title, $content, $api)
 {
-    $url = $api."/".$title."/".$content;
-    $push = curl_init();
-    $timeout = 5;
-    curl_setopt ($push, CURLOPT_URL, $url);
-    curl_setopt ($push, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt ($push, CURLOPT_CONNECTTIMEOUT, $timeout);
-    $result = curl_exec($push);
-    $result = json_decode($result, true);
-    curl_close($push);
+    $url = $api."/".urlencode($title)."/".urlencode($content);
+    $res = curl_get($url);
+    $result = json_decode($res, true);
     return $result;
 }
 
--- main.php
+++ main.php
@@ -92,26 +92,16 @@ getTaskID:
 $taskID = [];
 foreach ($course_list as $val) {
     $html = curl_get(sprintf(TASK_ID, $val['courseId'], $val['classId']), $jar_path);
-    $res = selector::select($html, '#startList div', 'css');
-
-    if (isset($res[0])) {
-        if (!is_array($res)) continue;
-        foreach ($res as $k => $v) {
-            $d = selector::select($v, '@activeDetail(.*?)"@', 'regex');
-            $d = str_replace(['(', ')', ''], '', $d);
-            $d = explode(",", $d);
-
-            if(isset($d[1])){
-                if (intval($d[1]) === 2) {
-                    $taskID[] = [
-                        $val['courseId'],//课程ID
-                        $val['classId'],//班级ID
-                        $d[0],//签到任务ID
-                        $val['title'],//课程名
-                        $val['teacherName'],//教师名
-                    ];
-                }
-            }
+    $res = json_decode($html, true)["activeList"];
+    foreach ($res as $k => $v) {
+        if($v["status"] == 1){
+             $taskID[] = [
+                $val['courseId'],//课程ID
+                $val['classId'],//班级ID
+                $v["id"],//签到任务ID
+                $val['title'],//课程名
+                $val['teacherName'],//教师名
+                ];
         }
     }
 }
