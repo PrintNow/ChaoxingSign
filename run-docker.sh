9f30ed729c8d3d387582123a1753859082e5e679
xiwangly2 <1334850101@qq.com>
2023-02-12 14:20:16 +0800

    1

--- run-docker.sh
+++ run-docker.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # dependence list: curl, cron, docker-compose,docker
 # 此脚本只需执行一次
-cd .
-mkdir -p /www/wwwroot/website
-cp -r www/wwwroot/config /www/wwwroot
-cp -r ChaoxingSign /www/wwwroot/website
-docker-compose up -d
-echo "*/3 * * * * /usr/bin/curl http://localhost/ChaoxingSign/index.php > /dev/null 2>&1" > /var/spool/cron/crontabs/root
-systemctl restart cron
+cd .;
+mkdir -p /www/wwwroot/website;
+cp -r www/wwwroot/config /www/wwwroot;
+cp -r ChaoxingSign /www/wwwroot/website;
+docker-compose up -d;
+echo "*/3 * * * * /usr/bin/curl http://localhost/ChaoxingSign/index.php > /dev/null 2>&1" > /var/spool/cron/crontabs/root;
+systemctl restart cron;
