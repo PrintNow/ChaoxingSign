440e3ea60d6cc8b726efe6106d0c294b323f2b7d
xiwangly2 <1334850101@qq.com>
2023-02-12 12:05:57 +0800

    适配Docker

--- run-docker.sh
+++ run-docker.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
-cp ChaoxingSign /www/wwwroot/website
+# dependence list: curl, cron, docker-compose,docker
+# 此脚本只需执行一次
+cd .
+mkdir -p /www/wwwroot/website
+cp -r www/wwwroot/config /www/wwwroot
+cp -r ChaoxingSign /www/wwwroot/website
 docker-compose up -d
 echo "*/3 * * * * /usr/bin/curl http://localhost/ChaoxingSign/index.php" | crontab -e
